ec00cfe82c6ef36f68e4ed2ee0f8d83b1a1bcc0a
ryan <337662980@qq.com>
2016-10-30 11:01:58 +0800

    and

---
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/App_Code/DependencyInstaller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Senparc.Weixin.MP.Sample.App_Code
+{
+    public class DependencyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(AllTypes.FromThisAssembly().BasedOn<IController>().LifestyleTransient());
+            //container.Register(null
+            //          /*  Component.For<ILogService>()
+            //                .ImplementedBy<LogService>()
+            //                .LifeStyle.PerWebRequest,
+
+            //            Component.For<IDatabaseFactory>()
+            //                .ImplementedBy<DatabaseFactory>()
+            //                .LifeStyle.PerWebRequest,
+            //            Component.For<IUnitOfWork>()
+            //                .ImplementedBy<UnitOfWork>()
+            //                .LifeStyle.PerWebRequest*/,
+
+            //            AllTypes.FromThisAssembly().BasedOn<IHttpController>().LifestyleTransient(),
+            //            AllTypes.FromThisAssembly().BasedOn<IController>().LifestyleTransient(),
+            //            AllTypes.FromAssemblyNamed("WebChat.Service")
+            //                .Where(type => type.Name.EndsWith("Service")).WithServiceAllInterfaces().LifestylePerWebRequest(),
+            //            AllTypes.FromAssemblyNamed("WebChat.Repository")
+            //                .Where(type => type.Name.EndsWith("Repository")).WithServiceAllInterfaces().LifestylePerWebRequest()
+
+            //            );
+
+
+        }
+    }
+}
---
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample/App_Code/WindsorActivator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+
+namespace Senparc.Weixin.MP.Sample.App_Code
+{
+    public class WindsorActivator : IHttpControllerActivator
+    {
+        private readonly IWindsorContainer container;
+        public WindsorActivator(IWindsorContainer container)
+        {
+            this.container = container;
+
+        }
+
+        public IHttpController Create(
+            HttpRequestMessage request,
+            HttpControllerDescriptor controllerDescriptor,
+            Type controllerType)
+        {
+            var controller =
+                (IHttpController)this.container.Resolve(controllerType);
+
+            request.RegisterForDispose(
+                new Release(
+                    () => this.container.Release(controller)));
+
+            return controller;
+        }
+
+        public IController Create(RequestContext requestContext, Type controllerType)
+        {
+            var controller = (IController)this.container.Resolve(controllerType);
+            return controller;
+
+        }
+        private class Release : IDisposable
+        {
+            private readonly Action release;
+
+            public Release(Action release)
+            {
+                this.release = release;
+            }
+
+            public void Dispose()
+            {
+                this.release();
+            }
+        }
+    }
+
+    
+}
