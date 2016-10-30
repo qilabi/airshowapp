15b05a238e8753cf92118ffc9d684d478e50e44d
hyihao <hyihao@163.com>
2016-10-30 13:13:45 +0800

    order entity

---
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/Data/Models/order_goods.cs
@@ -0,0 +1,98 @@
+using System;
+
+namespace Senparc.Weixin.MP.Sample.CommonService.Data.Models
+{
+    /// <summary>
+    /// 订单商品表
+    /// </summary>
+    [Serializable]
+    public  class order_goods
+    {
+        public order_goods()
+        { }
+        #region Model
+        private int _id;
+        private int _order_id = 0;
+        private int _goods_id = 0;
+        private string _goods_title = "";
+        private decimal _goods_price = 0M;
+        private decimal _real_price = 0M;
+        private int _quantity = 0;
+        private int _point = 0;
+        private DateTime _usedate;
+        /// <summary>
+        /// 自增ID
+        /// </summary>
+        public int id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 使用时间(出发日期)
+        /// </summary>
+        public DateTime useDate
+        {
+            set { _usedate = value; }
+            get { return _usedate; }
+        }
+        /// <summary>
+        /// 订单ID
+        /// </summary>
+        public int order_id
+        {
+            set { _order_id = value; }
+            get { return _order_id; }
+        }
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        public int goods_id
+        {
+            set { _goods_id = value; }
+            get { return _goods_id; }
+        }
+        /// <summary>
+        /// 商品标题
+        /// </summary>
+        public string goods_title
+        {
+            set { _goods_title = value; }
+            get { return _goods_title; }
+        }
+        /// <summary>
+        /// 商品价格
+        /// </summary>
+        public decimal goods_price
+        {
+            set { _goods_price = value; }
+            get { return _goods_price; }
+        }
+        /// <summary>
+        /// 实际价格
+        /// </summary>
+        public decimal real_price
+        {
+            set { _real_price = value; }
+            get { return _real_price; }
+        }
+        /// <summary>
+        /// 订购数量
+        /// </summary>
+        public int quantity
+        {
+            set { _quantity = value; }
+            get { return _quantity; }
+        }
+        /// <summary>
+        /// 积分,正数赠送|负数消费
+        /// </summary>
+        public int point
+        {
+            set { _point = value; }
+            get { return _point; }
+        }
+        #endregion Model
+
+    }
+}
