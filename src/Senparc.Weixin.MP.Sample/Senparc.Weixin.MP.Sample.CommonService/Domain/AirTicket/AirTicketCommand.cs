1aba6b4c3ec7f14366e5e4c0d2af5cfd91cad755
ryan <337662980@qq.com>
2016-11-01 23:41:04 +0800

    commit

--- src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/Domain/AirTicket/AirTicketCommand.cs
+++ src/Senparc.Weixin.MP.Sample/Senparc.Weixin.MP.Sample.CommonService/Domain/AirTicket/AirTicketCommand.cs
@@ -88,25 +88,6 @@ namespace Senparc.Weixin.MP.Sample.CommonService.Domain.AirTicket
             return result;
         }
 
-        public AirOrder GetOrder(string orderNo)
-        {
-            var sqlQuery = "select * from orders  where OrderNo=@OrderNo ";
-            var entity = default(AirOrder);
-            using (var conn = GetConnection())
-            {
-                entity = conn.Query<AirOrder>(sqlQuery, new {OrderNo = orderNo}).FirstOrDefault();
-                if (entity != null)
-                {
-                    var lines = conn.Query<PostOrderLineDto>("select * from orderLine where OrderNo=@OrderNo ",
-                        new {OrderNo = orderNo});
-                    foreach (var line in lines)
-                    {
-                        entity.Lines.Add(line);
-                    }
-                }
-            }
-            return entity;
-        }
 
 
     }
